d0c130ff73b5d36d507825b86592b9b607499079
Chris Shao <bjchris32@gmail.com>
2024-01-09 17:40:21 -0600

    use side effect to implement functional component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
+  console.log('render App');
+
   const [searchField, setSearchField] = useState(''); // [currentSate, setStateFunc]
-  console.log({searchField});
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  // the effect will execute without any dependent state
+  // So, it will only execute when mount and unmount
+  useEffect(() => {
+    console.log('fetch users!');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => setMonsters(users))
+  }, []);
+
+  // filter the monster when original monsters or search filed changes
+  useEffect(() => {
+    const newfilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newfilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchString = event.target.value.toLowerCase();
     setSearchField(searchString)
@@ -19,7 +41,7 @@ const App = () => {
         placeholder='Search monsters'
         onChangeHandler={onSearchChange}
       />
-      {/* <CardList cards={filteredMonsters}/> */}
+      <CardList cards={filteredMonsters}/>
     </div>
   );
 }
