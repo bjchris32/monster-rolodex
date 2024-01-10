75b29105237b285128f5b6c32e6a5338ecfaebdb
Chris Shao <bjchris32@gmail.com>
2024-01-09 21:26:42 -0600

    feat: convert class component to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,16 @@
-import { Component } from 'react';
 import './card-list.styles.css'
 import Card from '../card/card.component'
 
-class CardList extends Component {
-  render() {
-    const { cards } = this.props;
+const CardList = ({ cards }) => ( // props will be destructed to cards
+  // implicit return with (); instead of using {};
+  <div className='card-list'>
+    {cards.map((card) => {
+      return (
+        <Card card={card}/>
+      )
+    })}
+  </div>
+);
 
-    return (
-      <div className='card-list'>
-        {cards.map((card) => {
-          return (
-            <Card card={card}/>
-          )
-        })}
-      </div>
-    );
-  }
-}
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.card;
+const Card = ({ card }) => {
+  const { id, name, email } = card;
 
-    return (
-      <div className='card-container' key={id}>
-        <img
-          alt={`card ${name}`}
-          src={`https://robohash.org/${id}?set=set2&size=180x180`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    )
-  }
-}
+  return (
+    <div className='card-container' key={id}>
+      <img
+        alt={`card ${name}`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  )
+};
 
-export default Card
+export default Card;
